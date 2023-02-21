175e12259f131b3edddaf9c415c324024ae15907
tomadud-developer <tomadud045@student.polsl.pl>
2023-02-21 18:01:13 +0100

    initial ordersmicroservice, next step will be add api-gaeway and provide spring cloud support

---
+++ orders-microservice/src/main/java/com/ourkitchen/yourhealth/OrdersMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ourkitchen.yourhealth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersMicroserviceApplication.class, args);
+    }
+}
