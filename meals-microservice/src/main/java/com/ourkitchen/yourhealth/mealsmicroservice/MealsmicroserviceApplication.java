d0bf800534a038d0af4e186f149eef27aeac9764
tomadud-developer <tomadud045@student.polsl.pl>
2023-02-21 07:47:11 +0100

    Adding dependencies

---
+++ meals-microservice/src/main/java/com/ourkitchen/yourhealth/mealsmicroservice/MealsmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ourkitchen.yourhealth.mealsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsmicroserviceApplication.class, args);
+	}
+
+}
---
+++ meals-microservice/src/test/java/com/ourkitchen/yourhealth/mealsmicroservice/MealsmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourkitchen.yourhealth.mealsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
