47eaae44f731bea95d4db12f16a821ea51495438
tomdud-developer <tomadud045@student.polsl.pl>
2023-03-31 13:49:40 +0200

    Starting point of auth-service

---
+++ auth-service/src/main/java/com/ourkitchen/yourhealth/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.ourkitchen.yourhealth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @GetMapping("test")
+    String testController() {
+        return "test complete";
+    }
+
+}
