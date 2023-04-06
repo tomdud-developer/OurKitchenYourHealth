ddbcef9e39a41e4c764321e5582703026e0456d8
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-06 20:47:41 +0200

    Create Payment Microservice

---
+++ payment-microservice/src/main/java/com/ourkitchen/yourhealth/client/PayUClient.java
@@ -0,0 +1,11 @@
+package com.ourkitchen.yourhealth.client;
+
+import org.springframework.stereotype.Service;
+
+
+public interface PayUClient {
+
+
+
+
+}
---
+++ payment-microservice/src/main/java/com/ourkitchen/yourhealth/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.ourkitchen.yourhealth.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+
+
+
+}
