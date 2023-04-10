d90a55ef1c4b2a1060401e191ecad83569c29ef5
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-10 22:44:53 +0200

    Add OrderAddress

--- orders-microservice/src/main/java/com/ourkitchen/yourhealth/model/Order.java
+++ orders-microservice/src/main/java/com/ourkitchen/yourhealth/model/Order.java
@@ -31,4 +31,6 @@ public class Order {
     private Boolean isConfirmed;
 
     private OrderStatus orderStatus;
+
+    private OrderAddress orderAddress;
 }
---
+++ payment-microservice/src/main/java/com/ourkitchen/yourhealth/dto/OrderAddress.java
@@ -0,0 +1,19 @@
+package com.ourkitchen.yourhealth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderAddress {
+    private String street;
+    private String houseNumber;
+    private String flatNumber;
+    private String city;
+    private String postalCode;
+    private String country;
+}
