0dba9b07f29de9b74e5399c8b7fc2be2752737ac
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-13 13:08:49 +0200

    Create Restaurants microservice #2

---
+++ restuarant-microservice/src/main/java/yourhealth/RestaurantsMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package yourhealth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RestaurantsMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsMicroserviceApplication.class, args);
+    }
+
+
+}
