eb6a75d11057f18d45762fa6bde36d82336eb16f
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-13 13:12:15 +0200

    Create Restaurant model class

--- restuarant-microservice/src/main/java/yourhealth/model/Restaurant.java
+++ restuarant-microservice/src/main/java/yourhealth/model/Restaurant.java
@@ -1,4 +1,34 @@
 package yourhealth.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
 public class Restaurant {
+    @MongoId
+    private String id;
+    private String name;
+    private Address address;
+
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class Address {
+    private String street;
+    private String houseNumber;
+    private String flatNumber;
+    private String city;
+    private String postalCode;
+    private String country;
 }
